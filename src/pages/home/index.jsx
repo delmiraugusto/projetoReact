576a6cc6f0c04d54756151d1f5a389f72b16da62
Larissa Anunciação Teodoro <anunciacaoteodor@gmail.com>
2024-11-06 10:42:04 -0300

    Update index.jsx

--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -1,21 +1,20 @@
 import React, { useState, useEffect } from 'react';
-import axios from 'axios';
 import { CategorySelector } from '../../components/CategorySelector';
-
 import { ProductCard } from '../../components/ProductCard';
 import { fetchProducts } from '../../servicesAPI';
 import {
-  Header,
-  NavBar,
-  NavButton,
-  SearchContainer,
-  Container,
-  ProductsContainer,
-  SearchInput,
-  StyledLink
-} from './style';
+  Header, 
+  NavBar, 
+  NavButton, 
+  SearchContainer, 
+  Container, 
+  ProductsContainer, 
+  SearchInput, 
+  LogoContainer, 
+  Logo 
+} from './style'; 
 import { FaUserCircle, FaRegNewspaper, FaCartPlus, FaHistory, FaInfoCircle } from 'react-icons/fa';
-
+import logo from '../../assets/images/logo.png';
 
 export const Home = () => {
   const [selectedCategory, setSelectedCategory] = useState('Todos');
@@ -25,7 +24,6 @@ export const Home = () => {
   const [error, setError] = useState(null);
 
   const handleCategoryChange = (category) => setSelectedCategory(category);
-
   const handleSearchChange = (event) => setSearchTerm(event.target.value);
 
   const loadAPI = async () => {
@@ -52,17 +50,21 @@ export const Home = () => {
 
   return (
     <Container>
-
       <Header>
-        <NavBar>
-          <NavButton><FaUserCircle /> <StyledLink to="/login">Login</StyledLink></NavButton>
-          <NavButton><FaRegNewspaper /><StyledLink to="/cadastro">Cadastro</StyledLink></NavButton>
-          <NavButton><FaCartPlus /> <StyledLink to="/carrinho">Carrinho</StyledLink></NavButton>
-          <NavButton><FaHistory /> <StyledLink to="/historico">Histórico de Compras</StyledLink></NavButton>
-          <NavButton><FaInfoCircle /><StyledLink to="/sobre">Sobre nós</StyledLink></NavButton>
-        </NavBar>
+      
+        <LogoContainer>
+          <Logo src={logo} alt="Logo" />
+        </LogoContainer>
 
+        <NavBar>
+          <NavButton><FaUserCircle /> Login</NavButton>
+          <NavButton><FaRegNewspaper /> Cadastro</NavButton>
+          <NavButton><FaCartPlus /> Carrinho</NavButton>
+          <NavButton><FaHistory /> Histórico de compras</NavButton>
+          <NavButton><FaInfoCircle /> Sobre nós</NavButton>
+        </NavBar>
       </Header>
+
       <SearchContainer>
         <SearchInput
           type="text"
@@ -71,6 +73,7 @@ export const Home = () => {
           onChange={handleSearchChange}
         />
       </SearchContainer>
+
       <CategorySelector onCategoryChange={handleCategoryChange} />
       <ProductsContainer>
         {loading ? (
