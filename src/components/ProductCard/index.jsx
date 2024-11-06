432cb175fc57e434d166c14174725ae5e229b1b4
eric.s <ericcarvalhosilva03@gmail.com>
2024-11-06 13:21:14 -0300

    Modificando productCard para redirecionamento para detalhes do produto

--- src/components/ProductCard/index.jsx
+++ src/components/ProductCard/index.jsx
@@ -1,12 +1,20 @@
 import React from 'react';
+import { useNavigate } from 'react-router-dom';
 import { Card, Image, Name, Price, AddButton } from './style';
 
-export const ProductCard = ({ product }) => (
-  <Card>
-    <Image src={product.image} alt={product.name} />
-    <Name>{product.name}</Name>
-    <Price>R$ {product.price}</Price>
-    <AddButton>Adicionar ao Carrinho</AddButton>
-  </Card>
-);
+export const ProductCard = ({ product }) => {
+  const navigate = useNavigate();
 
+  const handleReadMore = () => {
+    navigate(`/products/${product.id}`);
+  };
+
+  return (
+    <Card>
+      <Image src={product.image} alt={product.name} />
+      <Name>{product.name}</Name>
+      <Price>R$ {product.price}</Price>
+      <AddButton onClick={handleReadMore}>Leia Mais</AddButton>
+    </Card>
+  );
+};
--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
+import { useNavigate } from 'react-router-dom';
 import { CategorySelector } from '../../components/CategorySelector';
 
 import { ProductCard } from '../../components/ProductCard';
@@ -79,7 +80,10 @@ export const Home = () => {
           <p>{error}</p>
         ) : (
           filteredProducts.map((product) => (
-            <ProductCard key={product.id} product={product} />
+            <ProductCard
+            key={product.id}
+            product={product}
+            />
           ))
         )}
       </ProductsContainer>
